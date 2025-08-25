7c6d64d55aa69b8c439c62c834a6e858b03ee042
Phil Labrum <phil@trymoab.com>
2025-08-25 09:41:28 -0400

    config bump

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  async rewrites() {
-    if (process.env.NODE_ENV === "production") return []; // let Vercel handle /api/*
+    if (process.env.NODE_ENV === "production") return [
+      { source: "/api/:path*", destination: "/api/:path*" },
+    ];
     return [
       { source: "/api/:path*", destination: "http://localhost:8000/api/:path*" },
     ];
