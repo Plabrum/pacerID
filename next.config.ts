09205bb70f2cec06eab071e6947d3edb8ce159a5
Phil Labrum <phil@trymoab.com>
2025-08-25 09:55:02 -0400

    trying things

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
  async rewrites() {
     if (process.env.NODE_ENV === "production") return [
-      { source: "/api/:path*", destination: "/api/:path*" },
+      { source: "/api/:path*", destination: "/api/" },
     ];
     return [
       { source: "/api/:path*", destination: "http://localhost:8000/api/:path*" },
